ebb952a05c7894a97aa49e0be17b605e6ea17217
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-28 06:04:00 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,36 @@
-Hashie
-Sinatra
-Octokit
-Pry
-AwesomePrint
+# source "https://rubygems.org"
+require_relative '../config/environment'
+# require "Gemfile"
+
+
+
+
+
+# require_relative '../bin/run.rb'
+
+# bundle install
+
+# Hashie
+# Sinatra
+# Octokit
+# Pry
+# AwesomePrint
+
+
+gem 'hashie'
+gem "sinatra",'1.4.4'
+gem "octokit", '~> 2.0'
+gem 'awesome_print', :git => 'git@github.com:awesome-print/awesome_print.git'
+
+
+
+
+
+
+# group :development do
+# gem "pry"
+# end
+
+# group :test do
+#   gem "rspec"
+# end
