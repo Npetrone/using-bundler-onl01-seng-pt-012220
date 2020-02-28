35b60ddb6bff9622ceb014e85204b319ccce7549
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-28 05:59:33 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -17,20 +17,20 @@ require "Gemfile"
 # AwesomePrint
 
 
-# gem 'hashie'
-# gem "sinatra",'1.4.4'
-# gem "octokit", '~> 2.0'
-# gem 'awesome_print', :git => 'git@github.com:awesome-print/awesome_print.git'
+gem 'hashie'
+gem "sinatra",'1.4.4'
+gem "octokit", '~> 2.0'
+gem 'awesome_print', :git => 'git@github.com:awesome-print/awesome_print.git'
 
 
 
 
 
 
-# group :development do
-# gem "pry"
-# end
+group :development do
+gem "pry"
+end
 
-# group :test do
-#   gem "rspec"
-# end
+group :test do
+  gem "rspec"
+end
